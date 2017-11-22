1b27870ec5ba1e83d598a1af54e1834d4b2b6e15
Richard West <r.west@northeastern.edu>
2017-11-21 23:56:20 -0500

    The 'stage_all' script will remove the previous max .csv file

--- stage_all.sh
+++ stage_all.sh
@@ -1,4 +1,6 @@
 
+# remove (from the repo not the disk) all the csv files.
+find binding_energies -name solver -execdir sh -c "ls solver/*.csv | xargs git rm --cached" \;
 # add just the highest numbered (last) solver csv
 find binding_energies -name solver -execdir sh -c "ls solver/*.csv | tail -n 1 | xargs git add" \;
 # add everything not ignored in the chemkin directories
