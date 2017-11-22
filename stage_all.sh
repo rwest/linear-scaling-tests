7708d83a852308bfc0526c5603098af16d88991c
Richard West <r.west@neu.edu>
2017-11-22 09:53:32 -0500

    UPdated the stage_all script

--- stage_all.sh
+++ stage_all.sh
@@ -1,6 +1,6 @@
 
-# remove (from the repo not the disk) all the csv files.
-find binding_energies -name solver -execdir sh -c "ls solver/*.csv | xargs git rm --cached" \;
+# remove (from the repo not the disk) all the solver csv files.
+find binding_energies -name solver -execdir sh -c "git rm -r --cached --ignore-unmatch solver" \;
 # add just the highest numbered (last) solver csv
 find binding_energies -name solver -execdir sh -c "ls solver/*.csv | tail -n 1 | xargs git add" \;
 # add everything not ignored in the chemkin directories
