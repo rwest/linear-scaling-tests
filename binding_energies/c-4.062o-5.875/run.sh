8721505e620c24bb936cadc5ba0e386b4d7e6bac
Emily Mazeau <mazeau.e@husky.neu.edu>
2018-09-26 14:57:37 -0400

    updated run.sh for all

--- binding_energies/c-4.062o-5.875/run.sh
+++ binding_energies/c-4.062o-5.875/run.sh
@@ -3,8 +3,8 @@
 #SBATCH --error=error.log
 #SBATCH --output=output.log
 #SBATCH -n1
-#SBATCH --mem=10Gb
-#SBATCH --time=2:00:00
+#SBATCH --mem=25Gb
+#SBATCH --time=5:00:00
 
 echo $RMGpy
 python  $RMGpy/rmg.py -p input.py
